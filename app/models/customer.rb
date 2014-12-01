2e0082758ae2e054d85df22b606b581a545924c4
Jim Withington <jamesrwithington@gmail.com>
2014-12-01 15:43:54 -0800

    Added scopes to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,6 @@
 class Customer < Account
   has_many :time_entries
+  scope :premier, -> { where("employees > ?", 60)}
+  scope :newest, -> { order("created_at DESC") }
+  scope :starts_with, ->(letter) {where("name LIKE ?", letter + "%")}
 end
